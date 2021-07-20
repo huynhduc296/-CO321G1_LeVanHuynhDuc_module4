ad7be2aa7600626a245e9e9bc24e1dbc1ae9076e
huynhduc296 <huynhduc290698@gmail.com>
2021-07-20 09:15:13 -1200

    commit

--- 03_views_Thymeleaf/exercise/product_management/src/main/java/com/example/controller/ProductController.java
+++ 03_views_Thymeleaf/exercise/product_management/src/main/java/com/example/controller/ProductController.java
@@ -17,49 +17,72 @@ import java.util.List;
 public class ProductController {
     @Autowired
     ProductService productService;
+
+
     @RequestMapping(value = "")
-    public ModelAndView view(){
+    public ModelAndView view() {
         List<Product> productList = productService.findAll();
-        return new ModelAndView("/list","products",productList);
+        return new ModelAndView("/list", "products", productList);
     }
+
+
     @GetMapping(value = "/create")
-    public ModelAndView showFormCreate(){
-        return new ModelAndView("/create","product",new Product());
+    public ModelAndView showFormCreate() {
+        return new ModelAndView("/create", "product", new Product());
     }
+
+
+
     @PostMapping(value = "/create")
-    public String save(@ModelAttribute Product product, RedirectAttributes redirectAttributes){
+    public String save(@ModelAttribute Product product, RedirectAttributes redirectAttributes) {
         product.setId((int) (Math.random() * 10000));
         this.productService.save(product);
-        redirectAttributes.addFlashAttribute("success","Create Success");
+        redirectAttributes.addFlashAttribute("success", "Create Success");
         return "redirect:/product/";
     }
+
+
+
     @GetMapping(value = "/edit")
-    public String showFormEdit(@RequestParam int id, Model model){
+    public String showFormEdit(@RequestParam int id, Model model) {
         Product product = this.productService.findById(id);
-        model.addAttribute("product",product);
+        model.addAttribute("product", product);
         return "/edit";
     }
+
+
+
     @PostMapping(value = "/edit")
-    public String updateProduct(@ModelAttribute Product product){
-        this.productService.update(product.getId(),product);
+    public String updateProduct(@ModelAttribute Product product) {
+        this.productService.update(product.getId(), product);
         return "redirect:/product/";
     }
+
+
+
     @GetMapping(value = "/delete")
-    public String delete(@RequestParam int id,Model model){
+    public String delete(@RequestParam int id, Model model) {
         Product product = this.productService.findById(id);
-        model.addAttribute("product",product);
+        model.addAttribute("product", product);
         return "/delete";
     }
+
+
+
     @PostMapping(value = "/delete")
-    public String remove(@ModelAttribute Product product){
+    public String remove(@ModelAttribute Product product) {
         this.productService.remove(product.getId());
         return "redirect:/product/";
     }
 
+
     @GetMapping(value = "/view")
-    public ModelAndView showInfo(@RequestParam int id){
-        return new ModelAndView("/view","product",productService.findById(id));
+    public ModelAndView showInfo(@RequestParam int id) {
+        return new ModelAndView("/view", "product", productService.findById(id));
     }
+
+
+
     @PostMapping(value = "/search")
     public ModelAndView search(@RequestParam String search) {
         return new ModelAndView("/list", "products", this.productService.searchByName(search));

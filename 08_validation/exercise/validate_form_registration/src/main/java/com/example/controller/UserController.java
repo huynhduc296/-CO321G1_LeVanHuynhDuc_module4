da8629800bdac3b5d338af3898b0b6fac45f40bf
your username <huynhduc290698@gmail.com>
2021-07-26 17:46:28 +0700

    commitbt

---
+++ 08_validation/exercise/validate_form_registration/src/main/java/com/example/controller/UserController.java
@@ -0,0 +1,43 @@
+package com.example.controller;
+
+import com.example.dto.UserDto;
+import com.example.model.entity.User;
+import com.example.model.service.IUserService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+import java.util.List;
+
+
+@Controller
+public class UserController {
+    @Autowired
+    IUserService iUserService;
+    @GetMapping("")
+    public String display(Model model){
+        model.addAttribute("userDto",new UserDto());
+        return "/index";
+
+    }
+    @PostMapping("/create")
+    public String create(@Valid @ModelAttribute UserDto userDto, BindingResult bindingResult, Model model){
+        new UserDto().validate(userDto,bindingResult);
+        if (bindingResult.hasFieldErrors()){
+            return "/index";
+        }else {
+            User user=new User();
+            BeanUtils.copyProperties(userDto,user);
+            iUserService.save(user);
+            List<User> list=iUserService.findAll();
+            model.addAttribute("users",list);
+            return "/list";      }
+
+    }
+}
---
+++ 08_validation/exercise/validate_form_registration/src/main/java/com/example/model/service/IUserService.java
@@ -0,0 +1,10 @@
+package com.example.model.service;
+
+import com.example.model.entity.User;
+
+import java.util.List;
+
+public interface IUserService {
+    void save(User user);
+    List<User> findAll();
+}

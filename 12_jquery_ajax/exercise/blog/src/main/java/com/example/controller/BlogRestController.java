23ad9dfae8042a7e0a7606f0c2b3b8a98f8676d3
your username <huynhduc290698@gmail.com>
2021-08-01 20:26:52 +0700

    commit

--- 12_jquery_ajax/exercise/blog/src/main/java/com/example/controller/BlogRestController.java
+++ 12_jquery_ajax/exercise/blog/src/main/java/com/example/controller/BlogRestController.java
@@ -5,6 +5,7 @@ import com.example.model.entity.Category;
 import com.example.model.service.IBlogService;
 import com.example.model.service.ICategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value = "/api/blogs")
 public class BlogRestController {
@@ -22,51 +24,52 @@ public class BlogRestController {
     @Autowired
     ICategoryService categoryService;
 
-    @GetMapping
-    public ResponseEntity<List<Blog>> findAllBlog(@RequestParam int page,@RequestParam Optional<String>name){
+    @GetMapping(value = "/category")
+    public ResponseEntity<Iterable<Category>> findAllCategory(){
+        List<Category> categoryList = (List<Category>) this.categoryService.findAll();
+        if (categoryList.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(categoryList,HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/blog-of-category/{id}")
+    public ResponseEntity<List<Blog>> findAllBlogByCategory(@PathVariable Long id){
+        List<Blog> blogList = this.blogService.findAllBlogByCategory(id);
+        if (blogList.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(blogList,HttpStatus.OK);
+    }
+    @GetMapping(value = "/detail/{id}")
+    public ResponseEntity<Blog> findBlogById(@PathVariable Integer id){
+        Blog blog = this.blogService.findById(id).orElse(null);
+        if (blog == null){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(blog,HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/search/{name}")
+    public ResponseEntity<List<Blog>> findBlogByName(@PathVariable String name){
+        List<Blog> blogList = this.blogService.findBlogByName(name);
+        if (blogList.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(blogList,HttpStatus.OK);
+    }
+
+    @GetMapping(value = {"/list"})
+    public ResponseEntity<Page<Blog>> goListBlog(@RequestParam int page,
+     @RequestParam Optional<String> name) {
         String names="";
         if(name.isPresent()){
             names=name.get();
         }
-        List<Blog> blogList = (List<Blog>) this.blogService.findAllByName(PageRequest.of(page,3),names);
-        if (blogList.isEmpty()) {
+        Page<Blog> blogs = this.blogService.findAllByName(PageRequest.of(page,3), names);
+        if (blogs.isEmpty()) {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
-        return new ResponseEntity<>(blogList, HttpStatus.OK);
-    }
-
-    @GetMapping(value = "/category")
-    public ResponseEntity<List<Category>> findAllCategory() {
-        List<Category> categoryList = (List<Category>) this.categoryService.findAll();
-        if (categoryList.isEmpty()) {
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        }
-        return new ResponseEntity<>(categoryList, HttpStatus.OK);
-    }
-
-    @GetMapping(value = "/blog-of-category/{id}")
-    public ResponseEntity<List<Blog>> findAllBlogByCategory(@PathVariable Long id) {
-        List<Blog> blogList = this.blogService.findAllBlogByCategory(id);
-        if (blogList.isEmpty()) {
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        }
-        return new ResponseEntity<>(blogList, HttpStatus.OK);
-    }
-
-    @GetMapping(value = "/detail/{id}")
-    public ResponseEntity<Blog> findBlogById(@PathVariable Integer id) {
-        Blog blog = this.blogService.findById(id).orElse(null);
-        if (blog == null) {
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        }
-        return new ResponseEntity<>(blog, HttpStatus.OK);
-    }
-    @GetMapping(value = "search/{name}")
-    public ResponseEntity<List<Blog>>findBlogByName(@PathVariable String name){
-        List<Blog>blogList=this.blogService.findBlogByName(name);
-        if(blogList.isEmpty()){
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        }
-        return new ResponseEntity<>(blogList,HttpStatus.OK);
+        return new ResponseEntity<>(blogs, HttpStatus.OK);
     }
 }

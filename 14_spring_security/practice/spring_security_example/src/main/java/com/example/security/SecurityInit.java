19e5afbc57ada1d0af4017efcee19899bf22948e
your username <huynhduc290698@gmail.com>
2021-08-04 15:54:07 +0700

    commit

--- 14_spring_security/practice/spring_security_example/src/main/java/com/example/security/SecurityInit.java
+++
@@ -1,6 +0,0 @@
-package com.example.security;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
-}

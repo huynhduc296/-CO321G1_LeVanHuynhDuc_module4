6359a3978782d3f25e89d39d06e337f34c42eacf
your username <huynhduc290698@gmail.com>
2021-08-04 10:31:05 +0700

    commit

---
+++ 14_spring_security/practice/spring_security_example/src/main/java/com/example/model/repository/AppUserRepository.java
@@ -0,0 +1,11 @@
+package com.example.model.repository;
+
+import com.example.model.entity.AppUser;
+import org.apache.catalina.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AppUserRepository extends JpaRepository<User,Long> {
+    AppUser findByUserName(String userName);
+}
